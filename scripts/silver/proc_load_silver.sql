7554dacb255c2654d9b8869c02e3fd88d1ce1ddb
DOUNIA TOUBAL <76491769+dounia4112@users.noreply.github.com>
2025-04-30 14:44:21 +0100

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,1 +1,162 @@
 
+-- solve the problem of duplicates by ranking them based on the cst_create_date and take the recent one
+-- trim the lastname & firstname to remove the space at the begining
+-- replace F with Female & M with Male & NULL with n/a in cst_gndr
+-- replace S with Single & M with Married & NULL with n/a in cst_marital_status
+
+TRUNCATE TABLEsilver.crm_cust_info
+
+INSERT INTO silver.crm_cust_info (
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_marital_status,
+	cst_gndr,
+	cst_create_date )
+SELECT 
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+	 WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+	 ELSE 'n/a'
+END cst_marital_status,
+CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+	 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+	 ELSE 'n/a'
+END cst_gndr,
+cst_create_date
+FROM (
+		SELECT * , ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+		FROM bronze.crm_cust_info
+		WHERE cst_id IS NOT NULL
+	  ) 
+WHERE flag_last = 1
+	  
+--select * from silver.crm_cust_info
+
+--==========================================================
+
+TRUNCATE TABLE silver.crm_prd_info
+
+INSERT INTO silver.crm_prd_info (
+  prd_id,
+  cat_id,
+  prd_key,
+  prd_nrm,
+  prd_cost,
+  prd_line,
+  prd_start_dt,
+  prd_end_dt
+)
+SELECT 
+  prd_id,
+  REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+  SUBSTRING(prd_key, 7, LENGTH(prd_key)) AS prd_key,
+  prd_nrm,
+  COALESCE(prd_cost, 0) AS prd_cost,
+  CASE UPPER(TRIM(prd_line))
+    WHEN 'M' THEN 'Mountain'
+    WHEN 'R' THEN 'Road'
+    WHEN 'S' THEN 'Other Sales'
+    WHEN 'T' THEN 'Touring'
+    ELSE 'n/a'
+  END AS prd_line,
+  CAST(prd_start_dt AS DATE) AS prd_start_dt,
+  CAST(
+    LEAD(prd_start_dt) OVER (
+      PARTITION BY prd_key 
+      ORDER BY prd_start_dt
+    ) - INTERVAL '1 day'
+    AS DATE
+  ) AS prd_end_dt  --create the end date based on the start of the next row
+FROM bronze.crm_prd_info;
+
+
+--select * from  silver.crm_prd_info
+
+
+--===========================================================
+
+TRUNCATE TABLE silver.crm_sales_details
+
+INSERT INTO silver.crm_sales_details (
+	sls_ord_num ,
+    sls_prd_key ,
+    sls_cust_id ,
+    sls_order_dt ,
+    sls_ship_dt ,
+    sls_due_dt ,
+    sls_sales ,
+    sls_quantity ,
+    sls_price
+)
+SELECT 
+	sls_ord_num ,
+    sls_prd_key ,
+    sls_cust_id ,
+	CASE WHEN sls_order_dt <= 0 OR LENGTH(CAST(sls_order_dt AS VARCHAR)) != 8 THEN NULL
+		ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+	END sls_order_dt,
+    CASE WHEN sls_ship_dt <= 0 OR LENGTH(CAST(sls_ship_dt AS VARCHAR)) != 8 THEN NULL
+		ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+	END sls_ship_dt,
+    CASE WHEN sls_due_dt <= 0 OR LENGTH(CAST(sls_due_dt AS VARCHAR)) != 8 THEN NULL
+		ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+	END sls_due_dt,
+    CASE WHEN sls_sales IS NULL OR sls_sales <=0 OR sls_sales != sls_quantity * ABS(sls_price)
+			THEN sls_quantity * ABS(sls_price)
+		 ELSE sls_sales
+	END sls_sales,
+    sls_quantity,
+    CASE WHEN sls_price IS NULL OR sls_price <=0
+		   THEN sls_sales / NULLIF(sls_quantity, 0)
+		 ELSE sls_price
+	END sls_price 
+FROM bronze.crm_sales_details
+
+
+--select * from  silver.crm_sales_details
+
+
+--=============================================
+
+TRUNCATE TABLE silver.erp_loc_a101
+
+INSERT INTO silver.erp_loc_a101 (
+	cid,
+	cntry
+)
+SELECT 
+	REPLACE(cid, '-', ''),
+	CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+		 WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+		 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+		 ELSE TRIM(cntry)
+	END cntry
+FROM bronze.erp_loc_a101
+
+--select * from  silver.erp_loc_a101
+
+
+--=============================================
+
+TRUNCATE TABLE silver.erp_px_cat_g1v2
+
+INSERT INTO silver.erp_px_cat_g1v2 (
+	id,
+	cat,
+	subcat,
+	maintenance
+)
+SELECT 
+	id, 
+	cat,
+	subcat,
+	maintenance
+FROM bronze.erp_px_cat_g1v2
+
+--select * from  silver.erp_px_cat_g1v2
+
